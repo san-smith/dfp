da535e3c6d6e09fdf149b8dd9c3fe48664e2976a
san-smith <san-smith@mail.ru>
2022-05-23 23:22:12 +0700

    Add functions

--- lib/src/functions.dart
+++ lib/src/functions.dart
@@ -1,3 +1,22 @@
-typedef Lazy<A> = A Function();
+import 'option.dart';
+import 'result.dart';
 
-typedef Predicate<A> = bool Function(A a);
+Option<A> option<A>(bool test, A value) => test ? Some(value) : None();
+
+Option<A> fromNullable<A>(A? a) => a != null ? Some(a) : None();
+
+Result<T, E> tryCatch<T, E>(T Function() f) {
+  try {
+    return Ok(f());
+  } on E catch (e) {
+    return Err(e);
+  }
+}
+
+Future<Result<T, E>> asyncTryCatch<T, E>(Future<T> f) async {
+  try {
+    return Ok(await f);
+  } on E catch (e) {
+    return Err(e);
+  }
+}
