77fe9e81c1d5ecffb236534a19a088ad720368b0
san-smith <san-smith@mail.ru>
2022-05-26 01:25:42 +0700

    Add tests for Option

---
+++ test/functions_test.dart
@@ -0,0 +1,31 @@
+import 'package:fp/src/functions.dart';
+import 'package:fp/src/result.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Functions', () {
+    test('option', () {
+      final o1 = option(true, 42);
+      final o2 = option(false, 42);
+
+      expect(o1.isSome, true);
+      expect(o2.isSome, false);
+    });
+
+    test('fromNullable', () {
+      int? a = 42;
+      int? b;
+
+      final s = fromNullable(a);
+      final n = fromNullable(b);
+
+      expect(s.isSome, true);
+      expect(n.isSome, false);
+    });
+
+    test('tryCatch', () {
+      expect(tryCatch(() => 42), Ok(42));
+      expect(tryCatch(() => throw 'Error'), Err('Error'));
+    });
+  });
+}
--- test/option_test.dart
+++ test/option_test.dart
@@ -1,53 +1,145 @@
-import 'package:fp/src/functions.dart';
 import 'package:fp/src/option.dart';
+import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+abstract class Test<T> {
+  void ifSome(T value);
+
+  void ifNone();
+}
+
+class TestMock<T> extends Mock implements Test<T> {}
+
 void main() {
-  test('isNone', () {
-    final n = None();
-    final s = Some(42);
+  group('Option', () {
+    test('isNone', () {
+      final n = None();
+      final s = Some(42);
 
-    expect(n.isNone, true);
-    expect(s.isNone, false);
-  });
+      expect(n.isNone, true);
+      expect(s.isNone, false);
+    });
 
-  test('isSome', () {
-    final n = None();
-    final s = Some(42);
+    test('isSome', () {
+      final n = None();
+      final s = Some(42);
 
-    expect(n.isSome, false);
-    expect(s.isSome, true);
-  });
+      expect(n.isSome, false);
+      expect(s.isSome, true);
+    });
 
-  test('option', () {
-    final o1 = option(true, 42);
-    final o2 = option(false, 42);
+    test('getOrElse', () {
+      final n = None();
+      final s = Some(42);
 
-    expect(o1.isSome, true);
-    expect(o2.isSome, false);
-  });
+      expect(n.getOrElse(0), 0);
+      expect(s.getOrElse(0), 42);
+    });
 
-  test('fromNullable', () {
-    int? a = 42;
-    int? b;
+    test('fold', () {
+      final n = None();
+      final s = Some(42);
 
-    final s = fromNullable(a);
-    final n = fromNullable(b);
+      expect(n.fold((a) => true, () => false), false);
+      expect(s.fold((a) => true, () => false), true);
+    });
 
-    expect(s.isSome, true);
-    expect(n.isSome, false);
-  });
+    test('map', () {
+      final n = None<int>();
+      final s = Some(42);
 
-  test('tryCatch', () {
-    expect(tryCatch(() => 42), Some(42));
-    expect(tryCatch(() => throw 'Error'), None());
-  });
+      expect(n.map((value) => value.toString()), None<String>());
+      expect(s.map((value) => value.toString()), Some('42'));
+    });
 
-  test('getOrElse', () {
-    final n = None();
-    final s = Some(42);
+    group('ifSome', () {
+      final s = Some(42);
+      final n = None<int>();
 
-    expect(n.getOrElse(0), 0);
-    expect(s.getOrElse(0), 42);
+      test('called once', () {
+        final mock = TestMock();
+        s.ifSome(mock.ifSome);
+        n.ifSome(mock.ifSome);
+
+        verify(mock.ifSome(42)).called(1);
+      });
+    });
+
+    group('ifNone', () {
+      final s = Some(42);
+      final n = None<int>();
+
+      test('called once', () {
+        final mock = TestMock();
+        s.ifNone(mock.ifNone);
+        n.ifNone(mock.ifNone);
+
+        verify(mock.ifNone()).called(1);
+      });
+    });
+
+    group('ifSomeElse', () {
+      final s = Some(42);
+      final n = None<int>();
+
+      test('ifSome called once', () {
+        final mock = TestMock();
+        s.ifSomeElse(mock.ifSome, mock.ifNone);
+        n.ifSomeElse(mock.ifSome, mock.ifNone);
+
+        verify(mock.ifSome(42)).called(1);
+      });
+
+      test('ifNone called once', () {
+        final mock = TestMock();
+        s.ifSomeElse(mock.ifSome, mock.ifNone);
+        n.ifSomeElse(mock.ifSome, mock.ifNone);
+
+        verify(mock.ifNone()).called(1);
+      });
+    });
+
+    group('when', () {
+      final s = Some(42);
+      final n = None<int>();
+
+      test('ifSome called twice', () {
+        final mock = TestMock();
+        s.when(some: mock.ifSome, none: mock.ifNone);
+        n.when(some: mock.ifSome, none: mock.ifNone);
+
+        s.when(some: mock.ifSome);
+        n.when(some: mock.ifSome);
+
+        verify(mock.ifSome(42)).called(2);
+      });
+
+      test('ifNone called once', () {
+        final mock = TestMock();
+        s.when(some: mock.ifSome, none: mock.ifNone);
+        n.when(some: mock.ifSome, none: mock.ifNone);
+
+        s.when(none: mock.ifNone);
+        n.when(none: mock.ifNone);
+
+        verify(mock.ifNone()).called(2);
+      });
+    });
+
+    test('toNullable', () {
+      final s = Some(42);
+      final n = None<int>();
+
+      expect(s.toNullable(), 42);
+      expect(n.toNullable(), null);
+    });
+
+    test('hasCode', () {
+      final s = Some(42);
+      final n = None<int>();
+
+      expect(s.hashCode, 42.hashCode);
+      expect(n.hashCode, 0);
+    });
   });
 }
